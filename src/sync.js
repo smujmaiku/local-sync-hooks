fb2e2809a43c11c5e4462b5e8a3aac1b92aa9cf8
Michael Szmadzinski <smujmaiku@gmail.com>
2020-09-11 23:47:03 -0400

    working not so prototype

--- src/sync.js
+++ src/sync.js
@@ -4,83 +4,96 @@
  * MIT Licensed
  */
 
-import { useEffect, useState, useCallback, useMemo } from 'react';
+import {
+	useEffect, useState, useCallback, useMemo,
+} from 'react';
 import localforage from 'localforage';
 
 const PACKAGE_NAME = 'someSyncHooks';
 
 const store = localforage.createInstance({ name: PACKAGE_NAME });
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export function useSyncState(key, defaultState) {
 	const channelId = `${PACKAGE_NAME}:${key}`;
 
 	const [state, setState] = useState(defaultState);
-	const [post, setPost] = useState(false);
-	const [time, setTime] = useState(0);
-	const [ready, setReady] = useState(false);
+	const [postMessage, setPostMessage] = useState(false);
+	const [remoteTime, setRemoteTime] = useState(0);
+	const [stateTime, setStateTime] = useState(0);
 
-	const channel = useMemo(() => {
-		const ch = new BroadcastChannel(channelId);
+	const ready = stateTime > 0;
+	const needGet = remoteTime >= stateTime;
 
-		ch.onmessage = ({ data }) => {
-			setTime(t => Math.max(t, data));
+	const channel = useMemo(() => new BroadcastChannel(channelId), [channelId]);
+
+	useEffect(() => {
+		channel.onmessage = ({ data }) => {
+			setRemoteTime((t) => Math.max(t, data));
 		};
 
-		return ch;
-	}, [channelId]);
-
-	useEffect(() => () => {
-		channel.onmessage = undefined;
-		channel.close();
+		return () => {
+			channel.onmessage = undefined;
+			channel.close();
+		};
 	}, [channel]);
 
 	const sendState = useCallback((data) => {
 		setState(data);
-		setTime(Date.now());
-		setPost(true);
-	}, [channel]);
+		setStateTime(Date.now());
+		setPostMessage(true);
+	}, []);
 
 	useEffect(() => {
-		if (!post) return;
-		let cancel;
+		if (!postMessage) return () => {};
 
+		let cancel;
 		(async () => {
 			await delay(10);
 			if (cancel) return;
+
 			await store.setItem(key, state);
-			channel.postMessage(time);
-			setPost(false);
+			if (cancel) return;
+
+			channel.postMessage(stateTime);
+			setPostMessage(false);
 		})();
 
 		return () => {
 			cancel = true;
 		};
-	}, [state, time, post]);
+	}, [channel, key, state, stateTime, postMessage]);
 
 	useEffect(() => {
-		if (time < 0) return;
-		let cancel;
+		if (!needGet) return () => {};
 
+		// Keep this to trigger start over on stateTime change
+		if (stateTime < 0) return () => {};
+
+		let cancel;
 		(async () => {
+			const now = Date.now();
+
 			await delay(10);
 			if (cancel) return;
+
 			const [data, exists] = await Promise.all([
 				store.getItem(key),
-				store.keys().then(keys => keys.some(k => k === key)),
+				store.keys().then((keys) => keys.some((k) => k === key)),
 			]);
 			if (cancel) return;
+
 			if (exists) {
 				setState(data);
 			}
-			setReady(true);
+			setStateTime(now);
 		})();
 
 		return () => {
 			cancel = true;
 		};
-	}, [key, time]);
+	}, [key, stateTime, needGet]);
 
 	return [state, sendState, ready];
 }
@@ -89,7 +102,7 @@ export function useSyncReducer(key, reducer, defaultState) {
 	const [state, setState, ready] = useSyncState(key, defaultState);
 
 	const sendDispatch = useCallback((action) => {
-		setState(s => reducer(s, action));
+		setState((s) => reducer(s, action));
 	}, [reducer, setState]);
 
 	return [state, sendDispatch, ready];
